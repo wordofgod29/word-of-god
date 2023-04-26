a99f2ac948b676b8f30acb8dc5071d8e9ca75b0e
wordofgod29 <131816248+wordofgod29@users.noreply.github.com>
2023-04-25 20:03:08 -0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,3 +1,53 @@
+#include "main.h"
+
+/**
+
+    get_flags - Calculates active flags
+
+    @format: Formatted string to check for flags
+
+    @i: Pointer to the current index in the format string
+
+    Return: An integer representing the active flags
+    */
+    int get_flags(const char *format, int *i)
+    {
+    int j, curr_i;
+    int flags = 0;
+    const char FLAG_CHARS[] = {'-', '+', '0', '#', ' ', '\0'};
+    const int FLAG_VALUES[] = {1, 2, 4, 8, 16, 0};
+		
+		for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+     {
+             for (j = 0; FLAG_CHARS[j] != '\0'; j++)
+             {
+                     if (format[curr_i] == FLAG_CHARS[j])
+                     {
+                             flags |= FLAG_VALUES[j];
+                             break;
+                     }
+             }
+
+             if (FLAG_CHARS[j] == 0)
+             {
+                     break;
+             }
+     }
+
+     *i = curr_i - 1;
+
+     return (flags);
+
+}
+
+
+
+
+
+
+
+
+
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
